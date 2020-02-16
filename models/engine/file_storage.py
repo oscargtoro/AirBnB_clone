c2caf5724ed42f480251bbbe82a9136d0764703e
oscargomez87 <oscarstiven87@gmail.com>
2020-02-16 18:49:34 -0500

    Fixed syntax for pep8 compliance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,13 +4,12 @@ import json
 import models
 from models.base_model import BaseModel
 
+
 class FileStorage:
     """class FileStorage"""
 
     __file_path = "file.json"
     __objects = {}
-
-
 
     def all(self):
         """all function"""
@@ -27,7 +26,7 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
 
         n_dict = {}
-        with open(self.__file_path, "w",encoding='utf-8') as f:
+        with open(self.__file_path, "w", encoding='utf-8') as f:
             for key, value in self.__objects.items():
                 n_dict[key] = value.to_dict()
             json.dump(n_dict, f)
