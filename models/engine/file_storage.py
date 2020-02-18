1f1446304e77f38b75c3c714757f335685c6ed1d
oscargomez87 <oscarstiven87@gmail.com>
2020-02-18 14:37:30 -0500

    Removed comment

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ module point 5 file_storage """
 import json
-#import models
+import models
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
