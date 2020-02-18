b2fe47500c6f6847fd63a1d7bb329f78f26230a4
oscargomez87 <oscarstiven87@gmail.com>
2020-02-18 13:39:11 -0500

    Made it executable

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ module point 5 file_storage """
 import json
-import models
+#import models
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
