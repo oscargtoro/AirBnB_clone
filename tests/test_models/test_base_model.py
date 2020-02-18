a5bf531666589ece6e5c20c76615c9f50a812cac
oscargomez87 <oscarstiven87@gmail.com>
2020-02-18 14:23:28 -0500

    Added a test and reworked directory into a package

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""unittests for this project"""
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Class with test cases for the Base class"""
+
+    def test_attrib(self):
+        """testing id counter and assignment"""
+        b1 = BaseModel()
+        b1.name = "Holberton"
+        b1.my_number = 89
+        self.assertAlmostEqual(b1.name, "Holberton")
+        self.assertAlmostEqual(b1.my_number, 89)
