9a11515b946bdd11be80a7023cb3b68772c3bc83
oscargomez87 <oscarstiven87@gmail.com>
2020-02-19 11:28:42 -0500

    Added more tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,10 +8,15 @@ class TestFileStorage(unittest.TestCase):
     """Class with test cases for the Place class"""
 
     def test_doc(self):
-        """Testing if doc exist """
+        """[storage] Testing if doc exist"""
         self.assertTrue(len(storage.__doc__) > 0)
         self.assertTrue(len(storage.all.__doc__) > 0)
         self.assertTrue(len(storage.new.__doc__) > 0)
         self.assertTrue(len(storage.save.__doc__) > 0)
         self.assertTrue(len(storage.__init__.__doc__) > 0)
         self.assertTrue(len(storage.reload.__doc__) > 0)
+
+    def test_all(self):
+        """[storage] Testing all method output"""
+        storageDict = storage.all()
+        self.assertEqual(type(storageDict), dict)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 """unittests for this project"""
 import unittest
+import os
 from models.user import User
 
 
 class TestUser(unittest.TestCase):
     """Class with test cases for the User class"""
 
-    def test_password(self):
-        """Test password assignment"""
+    def test_save(self):
+        """Test validation updated_at"""
         b1 = User()
-        b1.password = "123456789"
-        self.assertAlmostEqual(b1.password, "123456789")
+        up1 = b1.updated_at
+        b1.save()
+        up2 = b1.updated_at
+        b1.save()
+        self.assertNotEqual(up1, up2)
 
-    def test_email(self):
-        """[User] Testing email assignment"""
+    def test_instance(self):
+        """Test validation instance"""
         b1 = User()
-        b1.email = "test@prueba.com"
-        self.assertAlmostEqual(b1.email, "test@prueba.com")
+        self.assertIsInstance(b1, User)
 
-    def test_first_name(self):
-        """[User] Testing first_name assignment"""
-        b1 = User()
-        b1.first_name = "test"
-        self.assertAlmostEqual(b1.first_name, "test")
-
-    def test_last_name(self):
-        """[User] Testing Last_name assignment"""
-        b1 = User()
-        b1.last_name = "test1"
-        self.assertAlmostEqual(b1.last_name, "test1")
+    def test_access(self):
+        """Test access file"""
+        access = os.access("models/user.py", os.R_OK)
+        self.assertTrue(access, "Read permissions")
