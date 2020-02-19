fb5efaff239f0f1bd32d8e21290edcd0db938d7e
oscargomez87 <oscarstiven87@gmail.com>
2020-02-19 11:22:32 -0500

    Added more tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -7,16 +7,26 @@ from models.user import User
 class TestUser(unittest.TestCase):
     """Class with test cases for the User class"""
 
-    def test_attrib(self):
-        """Test attribute assignment"""
+    def test_password(self):
+        """Test password assignment"""
         b1 = User()
-        b1.password = "123456789"        
+        b1.password = "123456789"
         self.assertAlmostEqual(b1.password, "123456789")
 
-    def test_save(self):
+    def test_email(self):
+        """[User] Testing email assignment"""
         b1 = User()
-        up1 = b1.updated_at
-        b1.save()
-        up2 = b1.updated_at
-        b1.save()
-        self.assertNotEqual(up1, up2)
+        b1.email = "test@prueba.com"
+        self.assertAlmostEqual(b1.email, "test@prueba.com")
+
+    def test_first_name(self):
+        """[User] Testing first_name assignment"""
+        b1 = User()
+        b1.first_name = "test"
+        self.assertAlmostEqual(b1.first_name, "test")
+
+    def test_last_name(self):
+        """[User] Testing Last_name assignment"""
+        b1 = User()
+        b1.last_name = "test1"
+        self.assertAlmostEqual(b1.last_name, "test1")
