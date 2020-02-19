a416867b7081f5bc06f58f70c2ebe9b31c93cf17
oscargomez87 <oscarstiven87@gmail.com>
2020-02-19 11:35:14 -0500

    More tests added

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -8,6 +8,22 @@ class TestPlace(unittest.TestCase):
     """Class with test cases for the Place class"""
 
     def test_attrib(self):
-        """Test attribute assignment"""
+        """[Place] Test attribute assignment"""
         b1 = Place()
         self.assertAlmostEqual(b1.number_rooms, 0)
+
+    def test_doc(self):
+        """[Place] Testing if doc exist"""
+        self.assertTrue(len(Place.__doc__) > 0)
+        self.assertTrue(len(Place.__str__.__doc__) > 0)
+        self.assertTrue(len(Place.to_dict.__doc__) > 0)
+        self.assertTrue(len(Place.save.__doc__) > 0)
+        self.assertTrue(len(Place.__init__.__doc__) > 0)
+
+    def test_save(self):
+        """[Place] Testing save method"""
+        place = Place()
+        place1 = place.to_dict()
+        place.save()
+        place2 = place.to_dict()
+        self.assertNotEqual(place1["updated_at"], place2["updated_at"])
